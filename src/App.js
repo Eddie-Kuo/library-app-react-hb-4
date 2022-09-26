c265af3a3b7ed2443ebe27c2396176adde885e5e
Eddie-Kuo <Eddiekuo97@gmail.com>
2022-09-26 15:08:09 -0500

    added navigation from book list back home

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import BookList from './components/book/BookList';
 import './App.css';
 import Home from './components/home/Home';
 import { Route } from 'react-router-dom';
+import BookDetail from './components/book/BookDetail';
 
 function App() {
   // TODO: Add routes to books & views
   return (
     <main className="container">
       <Route exact path="/" component={Home} />
-      <Route exact path="/booklist" component={BookList} />
-
+      {/* <Route path="/booklist/:id" component={BookDetail} /> */}
+      <Route path="/booklist" component={BookList} />
+      {/* <Route path="*" component={Error} /> */}
     </main>
   );
 }
