7f678dc2dc2f5fb870c0dadf83b0708d46a27785
Eddie-Kuo <Eddiekuo97@gmail.com>
2022-09-26 14:59:55 -0500

    added home page + set up working route on App.js

---
+++ src/components/home/Home.js
@@ -0,0 +1,12 @@
+import { Link } from 'react-router-dom';
+import './Home.css';
+
+export default function Home() {
+  return (
+    <main className='home'>
+      <h1>Library Catalog</h1>
+      <h3>{`Welcome to Dangalf's Library of Computer Science`} </h3>
+      <h5>Check out the amazing selection of books in our <Link className='home' to="/booklist" >catalog</Link>.</h5>
+    </main>
+  );
+}
